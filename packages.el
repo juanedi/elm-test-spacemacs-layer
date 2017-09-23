dc3a6a13db1c3f258cd555f55fa57d1d5d2dcd98
Juan Edi <juan@noredink.com>
2017-09-22 21:28:50 -0300

    WIP: elm-test package and config layer

---
+++ packages.el
@@ -0,0 +1,28 @@
+;;; packages.el --- elm-test layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author: Juan Edi <jedi@chappie.local>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+;;; Code:
+
+(defconst elm-test-packages
+  '(
+    (elm-test :location local)
+    )
+  )
+
+(defun elm-test/init-elm-test ()
+  (use-package elm-test
+    :after elm-mode
+    :init
+    (spacemacs/set-leader-keys-for-major-mode 'elm-mode
+      "tb" 'elm-test-run
+      "tr" 'elm-test-rerun)))
+
+;;; packages.el ends here
