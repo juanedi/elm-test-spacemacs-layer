1a99170bc2b7c20e87997ba030c3bb92d2c04b68
Juan Edi <juan@noredink.com>
2018-08-30 09:41:54 -0300

    Use published elm-test-runner package from MELPA

--- packages.el
+++ packages.el
@@ -13,23 +13,23 @@
 
 (defconst elm-test-packages
   '(
-    (elm-test-el :location (recipe :fetcher github :repo "juanedi/elm-test-el"))
+    elm-test-runner
     )
   )
 
-(defun elm-test/init-elm-test-el ()
-  (use-package elm-test-el
+(defun elm-test/init-elm-test-runner ()
+  (use-package elm-test-runner
     :after elm-mode
     :init
     (progn
       (spacemacs/declare-prefix-for-mode 'elm-mode "mt" "elm/test")
       (spacemacs/set-leader-keys-for-major-mode 'elm-mode
-        "tb" 'elm-test-run
-        "td" 'elm-test-run-directory
-        "tp" 'elm-test-run-project
-        "tr" 'elm-test-rerun
-        "tw" 'elm-test-watch
-        "t TAB" 'elm-test-toggle-test-and-target
+        "tb" 'elm-test-runner-run
+        "td" 'elm-test-runner-run-directory
+        "tp" 'elm-test-runner-run-project
+        "tr" 'elm-test-runner-rerun
+        "tw" 'elm-test-runner-watch
+        "t TAB" 'elm-test-runner-toggle-test-and-target
         ))))
 
 ;;; packages.el ends here
